db51b81ef80a8b8aa66cb1f90bb88180656a8775
StephanYorchenko <yurchenko_s@tochka.com>
2023-01-17 22:32:26 +0500

    8.9. Оптимизируй это (Часть 1)

--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 import { ROUTES } from '../../app-routes.const';
 import { UserBlock } from '../../components/user-block/user-block';
 import NotFoundPage from '../not-found-page/not-found-page';
+import { PromoFilm } from '../../components/promo-film/promo-film';
 
 const MainPage = () => {
   const { movies: allMovies, activeGenre } = useAppSelector((state) => state);
@@ -27,10 +28,9 @@ const MainPage = () => {
   );
   const canLoadAdditionalItems = paginationCursor < allMovies.length;
 
-  if (!allMovies.length)
-  {return <NotFoundPage />;}
-
-  const { name, genre, releaseDate, posterImage } = allMovies[0];
+  if (!allMovies.length) {
+    return <NotFoundPage />;
+  }
 
   return (
     <>
@@ -67,10 +67,6 @@ const MainPage = () => {
       </div>
 
       <section className="film-card">
-        <div className="film-card__bg">
-          <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel"/>
-        </div>
-
         <h1 className="visually-hidden">WTW</h1>
 
         <header className="page-header film-card__head">
@@ -85,37 +81,7 @@ const MainPage = () => {
           <UserBlock />
         </header>
 
-        <div className="film-card__wrap">
-          <div className="film-card__info">
-            <div className="film-card__poster">
-              <img src={posterImage} alt="The Grand Budapest Hotel poster" width="218" height="327"/>
-            </div>
-
-            <div className="film-card__desc">
-              <h2 className="film-card__title">{name}</h2>
-              <p className="film-card__meta">
-                <span className="film-card__genre">{genre}</span>
-                <span className="film-card__year">{releaseDate}</span>
-              </p>
-
-              <div className="film-card__buttons">
-                <button className="btn btn--play film-card__button" type="button">
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s" />
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button className="btn btn--list film-card__button" type="button">
-                  <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add" />
-                  </svg>
-                  <span>My list</span>
-                  <span className="film-card__count">9</span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <PromoFilm />
       </section>
 
       <div className="page-content">
